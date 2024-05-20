f71cdadd474ce991bd53f234ded7d240c1a97fa0
Fly596 <69361885+Fly596@users.noreply.github.com>
2024-05-20 11:02:25 +0300

    + cinemas

--- app/src/main/java/com/example/kotlin_project_theater/data/TableData.kt
+++ app/src/main/java/com/example/kotlin_project_theater/data/TableData.kt
@@ -2,7 +2,24 @@ package com.example.kotlin_project_theater.data
 
 object Utils {
 
-    val cinemasList = listOf(Cinema(name = "nameq", location = "malholland drive"))
+    val cinemasList = listOf(
+        Cinema(
+            name = "AMC Empire 25",
+            location = "234 W 42nd St"
+        ),
+        Cinema(
+            name = "Regal Times Square",
+            location = "247 W 42nd St"
+        ),
+        Cinema(
+            name = "AMC Lincoln Square 13",
+            location = "1998 Broadway"
+        ),
+        Cinema(
+            name = "AMC 34th Street 14",
+            location = "312 W 34th St"
+        ),
+    )
 }
 
 enum class CarBody(val type: String = "") {
